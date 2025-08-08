30e97d3467e4b420ae4075f9f941471d6b6b44e2
twinboi90 <132564223+twinboi90@users.noreply.github.com>
2025-08-08 01:37:06 -0700

    Manually update SHA256 for v1.0.6

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -1,13 +1,17 @@
 class Zoomrecovery < Formula
   desc "Fix Zoom Error 1132 on macOS"
-  homepage "https://github.com/dbrowder23/ZoomRecovery"
-  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/null.tar.gz"
+  homepage "https://github.com/twinboi90/ZoomRecovery"
+  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.6.tar.gz"
   sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
   license "MIT"
 
   depends_on "spoof-mac"
 
   def install
+    # Replace version placeholder in the script
+    inreplace "zoomrecovery", "VERSION_PLACEHOLDER", version.to_s
+
+    # Install the script as an executable
     bin.install "zoomrecovery"
   end
 end
