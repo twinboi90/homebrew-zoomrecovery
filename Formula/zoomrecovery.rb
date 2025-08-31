49cace0dbfade45a478b3ecf78383afd193cabf8
twinboi90 <132564223+twinboi90@users.noreply.github.com>
2025-08-31 05:08:27 -0700

    Homebrew update to v1.0.9

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -2,7 +2,7 @@ class Zoomrecovery < Formula
   desc "Fix Zoom Error 1132 on macOS"
   homepage "https://github.com/twinboi90/ZoomRecovery"
   url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.9.tar.gz"
-  sha256 "dadc63a207af41a22009b76605115e93351e7dfd57072f21bd63fb7de890812dx"
+  sha256 "bda35c54e5167ed0b4148d8ddd439bc4c8f202cfc28b4e0ef3ddbfc496965c6a"
   license "MIT"
 
   depends_on "spoof-mac"
