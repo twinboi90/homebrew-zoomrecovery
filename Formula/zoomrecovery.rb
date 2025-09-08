03489074a836d963ff57a87252db15f8a4fd4a30
twinboi90 <132564223+twinboi90@users.noreply.github.com>
2025-09-08 02:32:28 -0700

    brew: bump zoomrecovery to v1.1.0

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -1,17 +1,14 @@
 class Zoomrecovery < Formula
   desc "Fix Zoom Error 1132 on macOS"
   homepage "https://github.com/twinboi90/ZoomRecovery"
-  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.9.tar.gz"
-  sha256 "bda35c54e5167ed0b4148d8ddd439bc4c8f202cfc28b4e0ef3ddbfc496965c6a"
+  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.1.0.tar.gz"
+  sha256 "aaf07bba6c8b152c072e5116cbebf5f513a366dbe73fbb8acb2b5dc71b675cc5"
+  version "1.1.0"
   license "MIT"
-
+  
   depends_on "spoof-mac"
 
   def install
-    # Replace version placeholder in the script
-    inreplace "zoomrecovery", "VERSION_PLACEHOLDER", version.to_s
-
-    # Install the script as an executable
     bin.install "zoomrecovery"
   end
 end
