a3d3e83cb7f67a9e3d7224b0a82cc2c517c2ac25
twinboi90 <132564223+twinboi90@users.noreply.github.com>
2025-09-09 05:32:29 -0700

    repaired ruby file

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -2,7 +2,7 @@ class Zoomrecovery < Formula
   desc "Reset Zoom state and spoof network identity for Zoom"
   homepage "https://github.com/twinboi90/ZoomRecovery"
   url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.1.1.tar.gz"
-  sha256 "3d6b3e4f418e4f395672e3236fd6ee84ca7c12eeb8dfab3bad634160e9f30b44
+  sha256 "3d6b3e4f418e4f395672e3236fd6ee84ca7c12eeb8dfab3bad634160e9f30b44"
   version "1.1.1"
 
   def install
