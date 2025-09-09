abc8d21dc1a06bf79884d3d93793d03e8652f1b5
twinboi90 <132564223+twinboi90@users.noreply.github.com>
2025-09-09 07:09:19 -0700

    bump homebrew to v1.0.10

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -1,9 +1,9 @@
 class Zoomrecovery < Formula
   desc "Reset Zoom state and spoof network identity for Zoom"
   homepage "https://github.com/twinboi90/ZoomRecovery"
-  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.9.tar.gz"
-  sha256 "bda35c54e5167ed0b4148d8ddd439bc4c8f202cfc28b4e0ef3ddbfc496965c6a"
-  version "1.0.9"
+  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.10.tar.gz"
+  sha256 "0568f772f1c7b6c32d08cb16761c6dc5ff4747a328221e2df1ca7edbce5f8ff5"
+  version "1.0.10"
 
   def install
     inreplace "zoomrecovery", "VERSION_PLACEHOLDER", version.to_s
