1e1796ed1f845d36d94d9799ad91693409de9782
twinboi90 <132564223+twinboi90@users.noreply.github.com>
2025-09-09 04:27:01 -0700

    Changes to be committed: 	modified:   zoomrecovery.rb

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -1,16 +1,18 @@
 class Zoomrecovery < Formula
-  desc "Fix Zoom Error 1132 on macOS"
+  desc "Reset Zoom state and spoof network identity"
   homepage "https://github.com/twinboi90/ZoomRecovery"
   url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.1..tar.gz"
   sha256 "3d6b3e4f418e4f395672e3236fd6ee84ca7c12eeb8dfab3bad634160e9f30b44"
-  license "MIT"
   version "1.1.1"
-
-  depends_on "spoof-mac""
+  license "MIT" # or your license
 
   def install
-    inreplace zoomrecovery, "VERSION_PLACEHOLDER", version.to_s
-    bin.install zoomrecovery
+    # If your script lives at the repo root:
+    inreplace "zoomrecovery", "VERSION_PLACEHOLDER", version.to_s
+    bin.install "zoomrecovery"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/zoomrecovery --version")
   end
 end
-
