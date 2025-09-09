e4a05b36295cf8683e61a1e472dc4dea7a960a6a
twinboi90 <132564223+twinboi90@users.noreply.github.com>
2025-09-09 05:28:50 -0700

    Changes to be committed: 	modified:   Formula/zoomrecovery.rb 	modified:   zoomrecovery

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -1,14 +1,13 @@
 class Zoomrecovery < Formula
-  desc "Reset Zoom state and spoof network identity"
+  desc "Reset Zoom state and spoof network identity for Zoom"
   homepage "https://github.com/twinboi90/ZoomRecovery"
-  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.1..tar.gz"
-  sha256 "3d6b3e4f418e4f395672e3236fd6ee84ca7c12eeb8dfab3bad634160e9f30b44"
+  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.1.1.tar.gz"
+  sha256 "3d6b3e4f418e4f395672e3236fd6ee84ca7c12eeb8dfab3bad634160e9f30b44
   version "1.1.1"
-  license "MIT" # or your license
 
   def install
-    # If your script lives at the repo root:
     inreplace "zoomrecovery", "VERSION_PLACEHOLDER", version.to_s
+    chmod 0755, "zoomrecovery"
     bin.install "zoomrecovery"
   end
 
