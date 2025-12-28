a09e1cdb6640d5ff100dfd040050ec4e96de30b1
twinboi90 <132564223+twinboi90@users.noreply.github.com>
2025-12-27 21:16:16 -0700

    update to v1.0.14

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -1,24 +1,19 @@
 class Zoomrecovery < Formula
   desc "Reset Zoom state and spoof network identity for Zoom"
   homepage "https://github.com/twinboi90/ZoomRecovery"
-  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.13.tar.gz"
-  sha256 "1429c64baaa002976c2db4475593f69db69ad10635e375bf90874d915d586f89"
-  version "1.0.13"
-  
+  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.14.tar.gz"
+  sha256 "ab7cdc7f55bcd47e8eb1a0c53d9febef1c7246a3e4bd54997224c420023c2caa"
+  version "1.0.14"
+
   depends_on "spoof-mac"
 
-inreplace "zoomrecovery" do |s|
-  replaced = false
-  [
-    /BAKED_VERSION\s*=\s*["'].*?["']/,
-    /BAKED_VERSION\s*=\s*[0-9]+\.[0-9]+(?:\.[0-9]+)?/,
-    /BAKED_VERSION\s*:\s*["'].*?["']/,
-    /BAKED_VERSION\s*[:=]\s*["']?.*?["']?/
-  ].each do |rx|
-    if s.sub!(rx, "BAKED_VERSION=\"#{version}\"")
-      replaced = true
-      break
-    end
+  def install
+    inreplace "zoomrecovery", "VERSION_PLACEHOLDER", version.to_s
+    chmod 0755, "zoomrecovery"
+    bin.install "zoomrecovery"
   end
-  s.prepend("BAKED_VERSION=\"#{version}\"\n") unless replaced
+
+  test do
+    assert_match "zoomrecovery #{version}", shell_output("#{bin}/zoomrecovery --version")
+  end
 end
