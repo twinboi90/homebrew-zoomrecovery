7807665d48087628b11e8a6d8df8be6e89636eab
Twin Boi <132564223+twinboi90@users.noreply.github.com>
2026-03-06 23:21:27 -0700

    v1.0.15
    updated spoof-mac requirements

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -1,11 +1,9 @@
 class Zoomrecovery < Formula
   desc "Reset Zoom state and spoof network identity for Zoom"
   homepage "https://github.com/twinboi90/ZoomRecovery"
-  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.14.tar.gz"
-  sha256 "ab7cdc7f55bcd47e8eb1a0c53d9febef1c7246a3e4bd54997224c420023c2caa"
-  version "1.0.14"
-
-  depends_on "spoof-mac"
+  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.15.tar.gz"
+  sha256 "0bcbbdcf0191748cfd0360adb9b6f5270d3cf44a7f2970920ec0d573586581e2"
+  version "1.0.15"
 
   def install
     inreplace "zoomrecovery", "VERSION_PLACEHOLDER", version.to_s
