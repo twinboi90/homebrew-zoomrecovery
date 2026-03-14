47e1e1280984ae863b7dac0055f806527c343544
Twin Boi <132564223+twinboi90@users.noreply.github.com>
2026-03-13 18:05:12 -0700

    update zoomrecovery to v1.0.17

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -1,9 +1,9 @@
 class Zoomrecovery < Formula
   desc "Reset Zoom state and spoof network identity for Zoom"
   homepage "https://github.com/twinboi90/ZoomRecovery"
-  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.16.tar.gz"
-  sha256 "7fcc9fce2aebb6b4b0ee92667486c3046779060c28b4619a5825bcb5a3ed26c8"
-  version "1.0.16"
+  url "https://github.com/twinboi90/ZoomRecovery/archive/refs/tags/v1.0.17.tar.gz"
+  sha256 "b7522e91a82c33a66ac2c19e32599fb5b2c1b6b3f9b7bf3ff4c8d3db7c5126b6"
+  version "1.0.17"
 
   def install
     inreplace "zoomrecovery", "VERSION_PLACEHOLDER", version.to_s
