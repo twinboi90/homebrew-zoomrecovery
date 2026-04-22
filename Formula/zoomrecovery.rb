2ca46b4e5cf1733b2f0ae5f847ae7ce3ae9ecdfc
Twin Boi <32564223+twinboi90@users.noreply.github.com>
2026-04-21 18:59:04 -0700

    Update zoomrecovery to v1.0.22

--- Formula/zoomrecovery.rb
+++ Formula/zoomrecovery.rb
@@ -2,7 +2,7 @@ class Zoomrecovery < Formula
   desc "Fix Zoom error 1132 by clearing corrupted database files"
   homepage "https://github.com/twinboi90/ZoomRecovery"
   url "https://github.com/twinboi90/ZoomRecovery/releases/download/v1.0.22/zoomrecovery"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "0f4615c2447664cb10bc62f9f0a8ab94d0ca5fb00915c05159dff5266c2d2b2b"
   license "MIT"
   depends_on :macos
 
